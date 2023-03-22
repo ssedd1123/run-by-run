ab6e177a9d353151980b6ffbc4a9ed9f03450d7a
tsangc <tommytsang3108@hotmail.com>
2023-03-21 23:34:40 -0400

    Added ability to use pre-enetered credential

--- runLog/shiftLog.py
+++ runLog/shiftLog.py
@@ -14,26 +14,69 @@ import os
 
 import sentiment as sen
 
-def getShiftLog(runs, timeSep=0.5):
+def autoLogin(driver, username, password, timeout):
+    print('*' * 100)
+    print('Using pre-entered credentials')
+    print('WARNING: Auto Login is Extremely unstable!')
+    print('Prepare for failure and try again')
+    try:
+        import keyboard
+    except ModuleNotFoundError as e:
+        print('*' * 100)
+        print('Keyboard module not found. Please install with \'pip install keyboard\'')
+        print('Abort. Please enter credentials manually.')
+        print('*' * 100)
+        return
+    # login with shiftLog2019 home page
+    # once you have the login session, you are all set
+    # if this url fails, replace with any other shift log page
+    url = 'https://online.star.bnl.gov/apps/shiftLog2019/logForFullTextSearch.jsp?text=20000000'
+
+    driver.get(url)
+    keyboard.write(username)
+    keyboard.press_and_release('tab')
+    keyboard.write(password)
+    keyboard.press_and_release("tab")
+    keyboard.press_and_release("enter")
+    print('*' * 100)
+    WebDriverWait(driver, timeout).until(EC.any_of(EC.title_is('ShiftLog'), EC.title_contains('Error'), EC.title_contains('error'), EC.title_contains('Unauthorize')))
+    if 'Unauthorize' in driver.title.lower():
+        raise RuntimeError('Incorrect password or username')
+
+
+def getShiftLog(runs, timeSep=0.5, username=None, password=None, firefox=False, timeout=60):
     result = {}
-    driver = webdriver.Chrome()
-    driver.set_page_load_timeout(10)
+    if firefox:
+        driver = webdriver.Firefox()
+    else:
+        driver = webdriver.Chrome()
+    driver.set_page_load_timeout(timeout)
+    if username is not None and password is not None: 
+        # supposedly you only need to enter credientials once at the beginning
+        autoLogin(driver, username, password, timeout)
+
     for i, runId in enumerate(runs):
         result[runId] = None
         year = int(str(runId)[:2]) - 1
         url = "https://online.star.bnl.gov/apps/shiftLog20%d/logForFullTextSearch.jsp?text=%d" % (year, runId)
         print('Loading data for run %d (%d/%d)' % (runId, i + 1, len(runs)))
+
+        try: 
+            driver.window_handles
+        except:
+            raise RuntimeError('Cannot call window handles. Browser may have been closed manually. Abort')
+            raise
         
         try:
             driver.get(url)
-            WebDriverWait(driver, 10).until(EC.any_of(EC.title_is('ShiftLog'), EC.title_contains('Error'), EC.title_contains('error')))
+            WebDriverWait(driver, timeout).until(EC.any_of(EC.title_is('ShiftLog'), EC.title_contains('Error'), EC.title_contains('error'), EC.title_contains('Unauthorize')))
         except:
             print('Connection time out for run %d' % runId)
             result[runId] = 'Connection to shift log time out'
             continue
         
         soup = BeautifulSoup(driver.page_source, "html.parser")
-        if 'error' in soup.title.get_text().lower():
+        if 'error' in soup.title.get_text().lower() or 'unauthorize' in soup.title.get_text().lower():
             print('Cannot load shift log for run %d' % runId)
             result[runId] = 'Cannot load shift log'
             continue
@@ -60,6 +103,7 @@ def getShiftLog(runs, timeSep=0.5):
         except Exception:
             pass
         time.sleep(timeSep)
+    driver.quit()
     return result
 
 def getRunIdFromFile(filename):
@@ -73,7 +117,7 @@ def getRunIdFromFile(filename):
             pass
     return runId
 
-def main(input, output, timeStep, allOutput):
+def main(input, output, timeStep, allOutput, **kwargs):
     _, ext = os.path.splitext(input)
     if ext == '.json':
         print('Reading json file %s' % input)
@@ -82,7 +126,7 @@ def main(input, output, timeStep, allOutput):
     else:
         print('Reading bad run list from text file %s' % input)
         runId = getRunIdFromFile(input)
-        result = getShiftLog(runId, timeStep)
+        result = getShiftLog(runId, timeStep, **kwargs)
     print('Saving shiftLog to %s' % output)
     with open(output, 'w') as f:
         json.dump(result, f)
@@ -111,9 +155,16 @@ if __name__ == '__main__':
     parser.add_argument('-no', '--negOutput', help='Name of the human readible file to which negative runs are stored')
     parser.add_argument('--useAI', action='store_true', help='Use AI to help select bad runs from shiftLog.')
     parser.add_argument('--justAI', action='store_true', help='Just Use AI to select bad runs from shiftLog. No user prompt. Will override --useAI option.')
-    args = parser.parse_args()
+    parser.add_argument('-un', '--username', help='Username of the shift log. (Experimental. Unstable. Use at your own risk)')
+    parser.add_argument('-pw', '--password', help='Password of the shift log. (Experimental. Unstable. Use at your own risk)')
+    parser.add_argument('--useFirefox', action='store_true', help='Switch to Firefox if desired. MAY NOT WORK WITH MANUAL CREDENTIALS.')
+    parser.add_argument('-to', '--timeout', default=60, type=float, help='Longest time to wait before connection timeout (default: %(default))')
 
-    main(args.input, args.output, args.timeStep, args.allOutput)
+
+    args = parser.parse_args()
+    main(args.input, args.output, args.timeStep, args.allOutput, 
+            username=args.username, password=args.password, 
+            firefox=args.useFirefox, timeout=args.timeout)
     print('*' * 100)
     sen.main(args.output, args.badrun, args.posOutput, args.negOutput, args.useAI, args.justAI)
     print('*' * 100)
